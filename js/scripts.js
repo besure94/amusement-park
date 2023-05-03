5f16a48ab27d3cd26e7738fc57eb922abbd39bdb
Brian Scherner <brianscherner@gmail.com>
2023-05-03 09:32:21 -0700

    Add window.onload function. Add onsubmit function targeting form. Add prevent.Default function and declare hideResults function. Define age as the age inputted into form. Define height as the height inputted into form.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ function hideResults() {
 	document.getElementById("tower").setAttribute("class", "hidden");
 	document.getElementById("sorry").setAttribute("class", "hidden");
 }
+
+window.onload = function() {
+	document.querySelector("form").onsubmit = function(event) {
+		event.preventDefault();
+		hideResults();
+		const age = parseInt(document.querySelector("input#age").value);
+		const height = parseInt(document.querySelector("input#height").value);
+	};
+};
