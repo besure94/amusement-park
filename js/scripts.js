79a7c8a2b5313d83f0dc7b45933f8974b930d17f
Brian Scherner <brianscherner@gmail.com>
2023-05-03 10:47:27 -0700

    edit hideResultsAndError function at top. Insert hideResultsAndError function under window.onload function before onsubmit function. Edit else if condition to include age && height condition. Edit else statement at bottom to include error message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function hideResultsAndErrors() {
+function hideResultsAndError() {
 	document.getElementById("error-message").setAttribute("class", "hidden");
 	document.getElementById("swings").setAttribute("class", "hidden");
 	document.getElementById("coaster").setAttribute("class", "hidden");
@@ -7,23 +7,29 @@ function hideResultsAndErrors() {
 }
 
 window.onload = function() {
+	hideResultsAndError();
+
 	document.querySelector("form").onsubmit = function(event) {
 		event.preventDefault();
 		hideResultsAndError();
 		const age = parseInt(document.querySelector("input#age").value);
 		const height = parseInt(document.querySelector("input#height").value);
 
-		if (age >= 12 && height >= 60) {
-			document.getElementById("swings").removeAttribute("class");
-			document.getElementById("coaster").removeAttribute("class");
-			document.getElementById("tower").removeAttribute("class");
-		} else if (age >= 12 || height >= 48) {
-			document.getElementById("swings").removeAttribute("class");
-			document.getElementById("coaster").removeAttribute("class");
-		} else if (age >= 6) {
-			document.getElementById("swings").removeAttribute("class");
+		if (age && height) {
+			if (age >= 12 && height >= 60) {
+				document.getElementById("swings").removeAttribute("class");
+				document.getElementById("coaster").removeAttribute("class");
+				document.getElementById("tower").removeAttribute("class");
+			} else if (age >= 12 || height >= 48) {
+				document.getElementById("swings").removeAttribute("class");
+				document.getElementById("coaster").removeAttribute("class");
+			} else if (age >= 6) {
+				document.getElementById("swings").removeAttribute("class");
+			} else {
+				document.getElementById("sorry").removeAttribute("class");
+			}
 		} else {
-			document.getElementById("sorry").removeAttribute("class");
+		document.getElementById("error-message").removeAttribute("class");
 		}
 	};
 };
