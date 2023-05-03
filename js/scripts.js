fdeea18d688d102079fa23bb6f17e4ab2d9ff73e
Brian Scherner <brianscherner@gmail.com>
2023-05-03 09:50:17 -0700

    Add else if statement setting conditions for each ride. If age is greater than or equal to 12 and height is greater than 60 inches, user can ride Tower ride and all others. If age is greater than or equal to 12 and height is greater than or equal to 48 inches, user can ride Coaster ride and Swings ride. If age is greater than or equal to 6, user can only ride Swings ride. If none of these conditions are met, user can not ride any of the rides.

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,18 @@ window.onload = function() {
 		hideResults();
 		const age = parseInt(document.querySelector("input#age").value);
 		const height = parseInt(document.querySelector("input#height").value);
+
+		if (age >= 12 && height >= 60) {
+			document.getElementById("swings").removeAttribute("class");
+			document.getElementById("coaster").removeAttribute("class");
+			document.getElementById("tower").removeAttribute("class");
+		} else if (age >= 12 || height >= 48) {
+			document.getElementById("swings").removeAttribute("class");
+			document.getElementById("coaster").removeAttribute("class");
+		} else if (age >= 6) {
+			document.getElementById("swings").removeAttribute("class");
+		} else {
+			document.getElementById("sorry").removeAttribute("class");
+		}
 	};
 };
