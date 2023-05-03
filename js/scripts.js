9de75c9fa6a886d354a88c70227517259a73ee39
Brian Scherner <brianscherner@gmail.com>
2023-05-03 09:27:19 -0700

    Add hideResults function for each div id and class.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function hideResults() {
+	document.getElementById("swings").setAttribute("class", "hidden");
+	document.getElementById("coaster").setAttribute("class", "hidden");
+	document.getElementById("tower").setAttribute("class", "hidden");
+	document.getElementById("sorry").setAttribute("class", "hidden");
+}
